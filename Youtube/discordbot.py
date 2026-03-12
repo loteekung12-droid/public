f3f86ee759242c06df54a9ffe6a52f860c608640
loteekung12-droid <loteekung12@gmail.com>
2026-03-12 21:27:09 +0700

    tee discordbot.py

--- Youtube/discordbot.py
+++ Youtube/discordbot.py
@@ -17,19 +17,18 @@ async def on_ready():
     print('Bot is running and has synced.')
 
 # Establish Command Name and Description 
-@bot.tree.command(name='***Enter a name***', description='***Enter a description***')
+@bot.tree.command(name='iplookup', description='my ip ดักพวกกระจอก')
 
 # Bot Prompt, API Call, and Response Functionality
-@app_commands.describe(user_input = "***Ener a Prompt for the user***: ")                 # Prompt User for Input
-async def bot_name(interaction: discord.Interaction, user_input: str):                 
+@app_commands.describe(user_input = "please enter ip addres: ")                # Prompt User for Input
+async def Ip_Lookup(interaction: discord.Interaction, user_input: str):                 
 
     # Use Requests to Obtain Data from API
-    url = f'***Enter an API Endpoint***{apikey}***Input Variable***{user_input}'
+    url = f'https://api.ipgeolocation.io/ipgeo?apiKey{apikey}&ip={user_input}'
     response = requests.get(url)
     json_response = response.json()
 
     # Send Message Containing Requested Data to User
-    await interaction.response.send_message(f'***Enter a message to send to user***', ephemeral=True)
-    return
-
+await interaction.response.send_message(f"IP: {json_response['ip']}\nCountry: {json_response['country_name']}\nISP: {json_response['isp']}",ephemeral=True)
+return
 bot.run(token) # Run Bot
